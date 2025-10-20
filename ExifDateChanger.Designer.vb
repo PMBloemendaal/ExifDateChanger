67695e61381e32495b3fb7dd47bebe198a1ff089
PMBloemendaal <PMBloemendaal@COO1>
2025-10-20 09:08:50 +0200

    Add project files.

---
+++ ExifDateChanger.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class ExifDateChanger
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        PictureBox1 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(12, 12)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 0
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Location = New Point(12, 41)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(100, 50)
+        PictureBox1.TabIndex = 1
+        PictureBox1.TabStop = False
+        ' 
+        ' ExifDateChanger
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(PictureBox1)
+        Controls.Add(Button1)
+        Name = "ExifDateChanger"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+
+End Class
---
+++ ExifDateChanger.vb
@@ -0,0 +1,77 @@
+﻿
+
+Option Strict On
+Option Explicit On
+
+Imports System.Drawing
+Imports System.IO
+Imports System.Windows.Forms
+
+Public Class ExifDateChanger
+
+    Private Sub KiesEnLaadJpgBestand()
+        ' We gebruiken de Using-statement om ervoor te zorgen dat de OpenFileDialog wordt vrijgegeven
+        Using ofd As New OpenFileDialog()
+
+            ' Configureer de dialoogbox
+            ofd.Filter = "JPEG Bestanden (*.jpg;*.jpeg)|*.jpg;*.jpeg|Alle Bestanden (*.*)|*.*"
+            ofd.Title = "Selecteer een JPG-bestand"
+
+            ' Haal het laatst gebruikte pad op uit My.Settings
+            Dim lastFilePath As String = My.Settings.LastFilePath
+
+            ' Controleer of er een geldig pad is opgeslagen en het bestand nog bestaat
+            If Not String.IsNullOrWhiteSpace(lastFilePath) AndAlso File.Exists(lastFilePath) Then
+                Try
+                    ' Stel de initiële map en bestandsnaam in
+                    ofd.InitialDirectory = Path.GetDirectoryName(lastFilePath)
+                    ofd.FileName = Path.GetFileName(lastFilePath)
+                Catch ex As Exception
+                    ' Indien Path.GetDirectoryName een fout geeft (bijv. ongeldig pad),
+                    ' gebruik een veilige standaardmap
+                    ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures)
+                End Try
+            Else
+                ' Indien er geen vorig pad is, gebruik dan de standaard Afbeeldingen-map
+                ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures)
+            End If
+
+            ' Toon het dialoogvenster en controleer of de gebruiker op OK heeft geklikt
+            If ofd.ShowDialog() = DialogResult.OK Then
+                Dim selectedFilePath As String = ofd.FileName
+
+                Try
+                    ' Laad het bestand
+                    LaadAfbeeldingInPictureBox(selectedFilePath)
+
+                    ' **Sla het nieuwe, succesvol geladen pad op**
+                    My.Settings.LastFilePath = selectedFilePath
+                    My.Settings.Save() ' Sla de wijzigingen op
+
+                Catch ex As Exception
+                    MessageBox.Show($"Fout bij het laden van de afbeelding: {ex.Message}", "Fout", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                End Try
+            End If
+        End Using
+    End Sub
+
+    ' Functie om de afbeelding daadwerkelijk te laden en weer te geven
+    Private Sub LaadAfbeeldingInPictureBox(ByVal filePath As String)
+        ' Eerst de oude afbeelding disposen om geheugen vrij te geven en het bestand te ontgrendelen
+        If PictureBox1.Image IsNot Nothing Then
+            PictureBox1.Image.Dispose()
+            PictureBox1.Image = Nothing
+        End If
+
+        ' Belangrijk: Gebruik New Bitmap(Image.FromFile(path)) om het bronbestand te ontgrendelen.
+        ' Dit voorkomt dat het JPG-bestand "in gebruik" blijft na het laden.
+        Using tempImage As Image = Image.FromFile(filePath)
+            PictureBox1.Image = New Bitmap(tempImage)
+        End Using
+
+        ' Pas PictureBox-eigenschappen aan
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        Me.Text = $"Afbeelding geladen: {Path.GetFileName(filePath)}"
+    End Sub
+
+End Class
